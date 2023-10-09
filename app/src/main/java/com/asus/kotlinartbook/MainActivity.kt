91fa752fc66bdff166977abf3c4f995bc6eaa5e0
Omer Eyibardakci <omereyibardakci@gmail.com>
2023-10-09 14:24:48 +0300

    Initial commit

---
+++ app/src/main/java/com/asus/kotlinartbook/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.asus.kotlinartbook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.asus.kotlinartbook.databinding.ActivityMainBinding
+
+
+private lateinit var binding: ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+
+    }
+
+
+}
